393d82d4a7d17e42a8f3b28681d416256d58b292
Danie <Danie@DESKTOP-R4RDJ95>
2022-01-10 19:29:35 +0100

    Commit en el que se configura correctamente EstadisticaLuzSolar

--- src/Ejer213/EstadisticaLuzSolar.java
+++ src/Ejer213/EstadisticaLuzSolar.java
@@ -1,6 +1,63 @@
 package Ejer213;
 
-public class EstadisticaLuzSolar{
-	private String[ ] nombresMeses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
-	
+
+/**
+ * Write a description of class EstadisticaLuzSolar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EstadisticaLuzSolar
+{
+    private String[ ] nombresMeses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+    int [ ] horas;
+
+    /**
+     * Constructor for objects of class EstadisticaLuzSolar
+     */
+    public EstadisticaLuzSolar()
+    {
+        
+        horas = new int[] {100, 90, 120, 150, 210, 250, 300, 310, 280, 230, 160, 120};
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public double getMediaSol()
+    {
+      double sumaHoras = 0;
+      for (int i = 0; i < nombresMeses.length; i++){
+          sumaHoras += horas[i];
+      }
+      
+      return sumaHoras / horas.length;
+    } 
+    
+    public String mesMasSoleado(){
+        int maximo = 0;
+        int mes = 0;
+        for (int i = 0; i < horas.length; i++){
+            if(horas[i] > maximo){
+                maximo = horas[i];
+                mes = i;
+            }
+        }
+        return nombresMeses[mes];
+    }
+    
+    public String mesMenosSoleado(){
+        int minimo = 2000;
+        int mes = 0;
+        for(int i = 0; i < horas.length; i++){
+            if(horas[i] < minimo){
+                minimo = horas[i];
+                mes = i;
+            }
+        }
+        return nombresMeses[mes];
+}
 }
