0f887e065653839f944fb42e7af64e879fb86090
Danie <Danie@DESKTOP-R4RDJ95>
2022-03-01 17:26:18 +0100

    Añadidos toString y getters y setters para Circuit

--- src/mario/Circuit.java
+++ src/mario/Circuit.java
@@ -1,5 +1,7 @@
 package mario;
 
+import java.util.Random;
+
 public class Circuit{
 	private String nombre;
 	private int distance;
@@ -10,5 +12,32 @@ public class Circuit{
 	this.distance = distance;
 }
 
+
+	public String getName() {
+		return nombre;
+	}
+
+
+	public void setName(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public int getDistance() {
+		return distance;
+	}
+
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Circuit [nombre=" + nombre + ", distance=" + distance + "]";
+	}
+
+	
 	
 }
