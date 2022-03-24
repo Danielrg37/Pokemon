f7e7462965e27baa46add58e1436c135060feeaf
Daniel <danielrg37@educastur.es>
2022-03-24 09:32:25 +0100

    Creada la clase para hacer los tests Primeros dos tests añadidos

---
+++ src/tarea_pruebas1/tarea1Test.java
@@ -0,0 +1,27 @@
+package tarea_pruebas1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class tarea1Test {
+
+	private tarea1 tarea;
+
+	@Test
+	void testlongitud() {
+		this.tarea = new tarea1();
+		int expected = 5;
+		int actual = this.tarea.longitud("Pacos");
+	}
+
+	
+	@Test
+	void testvocales() {
+		this.tarea = new tarea1();
+		int expected = 2;
+		int actual = this.tarea.vocales("Pacos");
+	}
+	
+	
+}
