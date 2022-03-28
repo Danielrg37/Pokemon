806efe7944920b03475747bbd6056a6302c3830f
Daniel <danielrg37@educastur.es>
2022-03-28 11:20:00 +0200

    Arreglado fallo en toString()

---
+++ src/ut5_20/CincoVeinte.java
@@ -0,0 +1,14 @@
+package ut5_20;
+
+public class CincoVeinte{
+	
+	
+	Integer unEntero = new Integer(2);
+	Integer [] arrayEnteros = { new Integer(1), new Integer(2), new Integer(3) };
+	
+	
+	
+	
+
+	
+}
