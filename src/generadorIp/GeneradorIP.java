815d976ad3282a9c48d1834b20414bc68aedd132
Danie <Danie@DESKTOP-R4RDJ95>
2022-03-29 21:31:10 +0200

    Tests

--- src/generadorIp/GeneradorIP.java
+++ src/generadorIp/GeneradorIP.java
@@ -1,5 +1,6 @@
 package generadorIp;
 
+import org.junit.jupiter.api.Test;
 
 public class GeneradorIP{
 	
@@ -8,13 +9,13 @@ public class GeneradorIP{
 
 	
 
-	
+	@Test
 	public int generarNumero(int min, int max) {
 		int numero = (int)(Math.random()* max + min);
 		return numero;
 	}
 	
-
+	@Test
 	public String generarCadena() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("GeneradorIP [min=");
